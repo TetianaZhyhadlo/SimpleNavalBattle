13b98f718cb86f9e10410fcf6e86bca345785f7e
Татьяна Жигадло <t.zhyhadlo@gmail.com>
2020-02-23 21:39:04 +0200

    Черновик

--- Program.cs
+++ Program.cs
@@ -4,25 +4,7 @@ namespace SimpleNavalBattle
 {
     class Program
     {
-        public void Shot(int a, int b)
-        {
-
-            if (myMap1[a, b] == 0)
-            {
-                myMap1[a, b] = -1;
-                Console.WriteLine("Ooops! Slip-up.");
-                Console.WriteLine("Press 0 to change player.");
-                if (nextPlayer == '0')
-                    Console.Clear();
-            }
-            else
-            {
-                myMap1[a, b] = 2;
-                Shot(a, b);
-            }
-
-
-        }
+       
         static void Main(string[] args)
         {
             Console.WriteLine("Enter your name:");
@@ -228,69 +210,28 @@ namespace SimpleNavalBattle
             {
                 case "1":
                     {
-                        for (int i = 0; i < mapSize - 1; i++)
-                        {
-                            Console.WriteLine();
-                            for (int k = 0; k < mapSize - 1; k++)
-                            {
-
-                                Console.Write($"\t|{map1[i, k]}");
-                            }
-                        }
                         myMap1 = map1;
                         break;
                     }
                 case "2":
                     {
-                        for (int i = 0; i < mapSize - 1; i++)
-                        {
-                            Console.WriteLine();
-                            for (int k = 0; k < mapSize - 1; k++)
-                            {
-
-                                Console.Write($"\t|{map2[i, k]}");
-                            }
-                        }
                         myMap1 = map2;
                         break;
                     }
                 case "3":
                     {
-                        for (int i = 0; i < mapSize - 1; i++)
-                        {
-                            Console.WriteLine();
-                            for (int k = 0; k < mapSize - 1; k++)
-                            {
-
-                                Console.Write($"\t|{map3[i, k]}");
-                            }
-                        }
                         myMap1 = map3;
                         break;
                     }
                 case "4":
                     {
-                        for (int i = 0; i < mapSize - 1; i++)
-                        {
-                            Console.WriteLine();
-                            for (int k = 0; k < mapSize - 1; k++)
-                            {
-
-                                Console.Write($"\t|{map4[i, k]}");
-                            }
-                        }
                         myMap1 = map4;
                         break;
                     }
-
-
-
             }
-            Console.WriteLine();
             int[,] player1Enemy = new int[mapSize, mapSize];
             for (int i = 0; i < mapSize - 1; i++)
             {
-                Console.WriteLine();
                 for (int k = 0; k < mapSize - 1; k++)
                 {
 
@@ -300,17 +241,285 @@ namespace SimpleNavalBattle
                         player1Enemy[i,k] = k;
                     else
                         player1Enemy[i, k] = 0;
+                }
+            }
+            int[,] myMap2 = new int[mapSize, mapSize];
+            switch (mapNum2)
+            {
+                case "1":
+                    {
+                        myMap2 = map1;
+                        break;
+                    }
+                case "2":
+                    {
+                        myMap2 = map2;
+                        break;
+                    }
+                case "3":
+                    {
+                        myMap2 = map3;
+                        break;
+                    }
+                case "4":
+                    {
+                        myMap2 = map4;
+                        break;
+                    }
+            }
+            Console.WriteLine();
+            int[,] player2Enemy = new int[mapSize, mapSize];
+            for (int i = 0; i < mapSize - 1; i++)
+            {
+                for (int k = 0; k < mapSize - 1; k++)
+                {
 
-                    Console.Write($"\t|{player1Enemy[i,k]}");
+                    if (k == 0 && i != 0)
+                        player2Enemy[i, k] = i;
+                    else if (k != 0 && i == 0)
+                        player2Enemy[i, k] = k;
+                    else
+                        player2Enemy[i, k] = 0;
 
                 }
             }
-            Console.WriteLine("Enter coordinates for shooting:");
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            Shot(a,b);
+            int count;
+            do
+            {
+                count = 0;
+                Console.WriteLine("My map");
+                for (int i = 0; i < mapSize - 1; i++)
+                {
+                    Console.WriteLine();
+                    for (int k = 0; k < mapSize - 1; k++)
+                    {
+                        Console.Write($"\t{myMap1[i, k]}");
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("Enemy Map");
+                for (int i = 0; i < mapSize - 1; i++)
+                {
+                    Console.WriteLine();
+                    for (int k = 0; k < mapSize - 1; k++)
+                    {
+                        Console.Write($"\t{player1Enemy[i, k]}");
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("Enter coordinates for shooting:");
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                Shot1(a, b);
+                Console.WriteLine();
+                Console.WriteLine("Press 0 to change player.");
+                nextPlayer = Convert.ToChar(Console.ReadLine());
+                if (nextPlayer == '0')
+                    Console.Clear();
+                Console.WriteLine("My map");
+                for (int i = 0; i < mapSize - 1; i++)
+                {
+                    Console.WriteLine();
+                    for (int k = 0; k < mapSize - 1; k++)
+                    {
+                        Console.Write($"\t{myMap2[i, k]}");
+                        if (myMap2[i, k] == 1)
+                            count++;
+                    }
+                }
+                if(count==0)
+                {
+                    Console.WriteLine("Player number 1 is winner!");
+                    break;
+                }
+                count = 0;
+                Console.WriteLine();
+                Console.WriteLine("Enemy Map");
+                for (int i = 0; i < mapSize - 1; i++)
+                {
+                    Console.WriteLine();
+                    for (int k = 0; k < mapSize - 1; k++)
+                    {
+                        Console.Write($"\t{player2Enemy[i, k]}");
+                    }
+                }
 
-            
+                Console.WriteLine();
+                Console.WriteLine("Enter coordinates for shooting:");
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+                Shot2(a, b);
+                for (int i = 0; i < mapSize - 1; i++)
+                {
+                    Console.WriteLine();
+                    for (int k = 0; k < mapSize - 1; k++)
+                    {
+                        if (myMap1[i, k] == 1)
+                            count++;
+                    }
+                }
+                if (count == 0)
+                {
+                    Console.WriteLine("Player number 2 is winner!");
+                    break;
+                }
+                Console.WriteLine();
+                Console.WriteLine("Press 0 to change player.");
+                nextPlayer = Convert.ToChar(Console.ReadLine());
+                if (nextPlayer == '0')
+                    Console.Clear();
+            }
+            while (count != 0);
+            void Shot1(int a, int b)
+            {
+
+                if (myMap2[a, b] == 0)
+                {
+                    myMap2[a, b] = -1;
+                    player1Enemy[a, b] = -1;
+                    Console.Clear();
+                    Console.WriteLine("My Map");
+                    for (int i = 0; i < mapSize - 1; i++)
+                    {
+                        Console.WriteLine();
+                        for (int k = 0; k < mapSize - 1; k++)
+                        {
+
+                            Console.Write($"\t|{myMap1[i, k]}");
+
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Enemy Map");
+                    for (int i = 0; i < mapSize - 1; i++)
+                    {
+                        Console.WriteLine();
+                        for (int k = 0; k < mapSize - 1; k++)
+                        {
+                            Console.Write($"\t|{player1Enemy[i, k]}");
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Ooops! Slip-up.");    
+                }
+                else if (myMap2[a, b] == 1)
+                {
+                    myMap2[a, b] = 2;
+                    player1Enemy[a, b] = 2;
+                    Console.Clear();
+                    Console.WriteLine("My Map");
+                    for (int i = 0; i < mapSize - 1; i++)
+                    {
+                        Console.WriteLine();
+                        for (int k = 0; k < mapSize - 1; k++)
+                        {
+
+                            Console.Write($"\t|{myMap1[i, k]}");
+
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Enemy Map");
+                    for (int i = 0; i < mapSize - 1; i++)
+                    {
+                        Console.WriteLine();
+                        for (int k = 0; k < mapSize - 1; k++)
+                        {
+                            Console.Write($"\t|{player1Enemy[i, k]}");
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Good shot!");
+                    Console.WriteLine("Enter coordinates for shooting:");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Shot1(a, b);   
+                }
+                else
+                {
+                    Console.WriteLine("You've already shooted here, try again.");
+                    Console.WriteLine("Enter coordinates for shooting:");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Shot1(a, b);
+                }
+                
+            }
+            void Shot2(int a, int b)
+            {
+                if (myMap1[a, b] == 0)
+                {
+                    myMap1[a, b] = -1;
+                    player2Enemy[a, b] = -1;
+                    Console.Clear();
+                    Console.WriteLine("My Map");
+                    for (int i = 0; i < mapSize - 1; i++)
+                    {
+                        Console.WriteLine();
+                        for (int k = 0; k < mapSize - 1; k++)
+                        {
+
+                            Console.Write($"\t|{myMap2[i, k]}");
+
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Enemy Map");
+                    for (int i = 0; i < mapSize - 1; i++)
+                    {
+                        Console.WriteLine();
+                        for (int k = 0; k < mapSize - 1; k++)
+                        {
+                            Console.Write($"\t|{player2Enemy[i, k]}");
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Ooops! Slip-up.");
+                }
+                else if (myMap1[a, b] == 1)
+                {
+                    myMap1[a, b] = 2;
+                    player2Enemy[a, b] = 2;
+                    Console.Clear();
+                    Console.WriteLine("My Map");
+                    for (int i = 0; i < mapSize - 1; i++)
+                    {
+                        Console.WriteLine();
+                        for (int k = 0; k < mapSize - 1; k++)
+                        {
+
+                            Console.Write($"\t|{myMap2[i, k]}");
+
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Enemy Map");
+                    for (int i = 0; i < mapSize - 1; i++)
+                    {
+                        Console.WriteLine();
+                        for (int k = 0; k < mapSize - 1; k++)
+                        {
+                            Console.Write($"\t|{player2Enemy[i, k]}");
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Good shot!");
+                    Console.WriteLine("Enter coordinates for shooting:");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Shot2(a, b);
+                }
+                else
+                {
+                    Console.WriteLine("You've already shooted here, try again.");
+                    Console.WriteLine("Enter coordinates for shooting:");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Shot2(a, b);
+                }
+                
+            }
         }
+        
     }
 }
